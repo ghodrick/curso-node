871dc87e85f67354497db598b21fa187c18363e6
Ghodrick <ghodrickdev@gmail.com>
2024-03-23 11:25:06 +0100

    Añadir y testear winston

--- 02-Bases/src/app.js
+++ 02-Bases/src/app.js
@@ -1,4 +1,13 @@
 const { getPokemonByID } = require("./js-foundation/06-promises");
 
-getPokemonByID(4).then(name => console.log(name));
+const buildLogger = require('./libraries/logger.adapter');
+
+const logger = buildLogger('app.js');
+
+logger.log('Hola mundo');
+logger.error('Esto es un error');
+
+// getPokemonByID(4)
+// .then(name => console.log(name))
+// .catch(err => console.log('El error es', err.message))
 
